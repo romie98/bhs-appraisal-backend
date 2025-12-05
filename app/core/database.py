e95fa64dc670474ea94e85bcc41a1b167dce9ac6
Romario Cohen <romariocohen98@gmaail.com>
2025-12-04 21:33:57 -0500

    Fix os import and use settings.DATABASE_URL

--- app/core/database.py
+++ app/core/database.py
@@ -1,10 +1,11 @@
 """Database configuration and session management"""
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
